f412cc91248a1ff94fdb202dfb410679b4331c35
DZI-dotWRLD <danila.ishchanka@gmail.com>
2026-06-10 15:00:16 -0400

    Task1

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -99,5 +99,36 @@ class Enrollment(models.Model):
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
 #class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+
+
+
+
+class Question(models.Model):
+    content = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.IntegerField(default=50)
+
+
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return "Question: " + self.content
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+    
